cf74c9e96747cd531b5f941f4151d9a1c8f6e06a
Débora Castro <deboracastro.pm@gmail.com>
2021-03-12 19:53:06 -0300

    chore: configuraçao para swagger

--- apps/ms-cliente/public/documentacao/api.php
+++ apps/ms-cliente/public/documentacao/api.php
@@ -3,3 +3,4 @@ require("../../vendor/autoload.php");
 $openapi = \OpenApi\scan('../../app/Http/Controllers/APIv1');
 header('Content-Type: application/json');
 echo $openapi->toJson();
+//composer require tymon/jwt-auth
